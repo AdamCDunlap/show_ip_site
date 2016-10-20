35aea868326cf6062c753343972f6444a6b0c511
Adam Dunlap <adunlap@hmc.edu>
2016-10-20 00:49:34 -0700

    Minor bug fixes and cleanup

--- show_ip_site.js
+++ show_ip_site.js
@@ -1,22 +1,35 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 
 // curl -d 'message you want to send' IP_ADDRESS:3142
+
+var max_msg_len = 100;
+var max_num_msgs = 100;
+
+app.use(express.static('static'));
 
 app.set('view engine', 'pug');
 app.enable('trust proxy');
 
-var ips = []
+var msgs = []
 
 app.post('/', function(req, res) {
     var ip = req.connection.remoteAddress;
+    var fullData = '';
+
     req.on('data', function(data) {
-        if (data.length > 100) {
-          data = data.slice(0, 100);
+        if (fullData.length < max_msg_len) {
+            fullData += data;
+            if (fullData.length > max_msg_len) {
+              fullData = fullData.slice(0, max_msg_len);
+            }
         }
+    });
+    req.on('end', function() {
         //console.log(ip);
-        ips.push({ip: ip, time: new Date(), data: data});
-        if (ips.length > 100) {
-            ips = ips.slice(-100);
+        msgs.push({ip: ip, time: new Date(), data: fullData});
+        if (msgs.length > max_num_msgs) {
+            msgs = msgs.slice(-max_num_msgs);
         }
         res.send('Got your IP: ' + ip + '\n');
     });
@@ -25,16 +38,16 @@ app.post('/', function(req, res) {
 app.get('/', function(req, res) {
     var ip = req.connection.remoteAddress;
     if (ip.substring(0,7) != '134.173') {
-        console.log('Rejected ' + ip);
+        //console.log('Rejected ' + ip);
         res.send('For security, this site can only be reached from Claremont\n')
     } else {
         var now = new Date();
         // Difference of dates gives a difference in milliseconds. Clear
         // entries once they're 10 minutes old
-        while (ips.length > 0 && now - ips[0].time > 10*60*1000) {
-            ips.shift();
+        while (msgs.length > 0 && now - msgs[0].time > 10*60*1000) {
+            msgs.shift();
         }
-        res.render('template', {ips: ips});
+        res.render('mainpage', {msgs: msgs});
     }
 });
 
